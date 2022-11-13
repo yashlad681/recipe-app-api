db2a3bafc97fc87cdbb5e06105c7e964cbae27f1
yashlad681 <yashlad681@gmail.com>
2022-11-13 12:42:14 +0530

    Linting Changes

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,11 +3,11 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         res = calc.subtract(10, 15)
 
-        self.assertEqual(res,5)    
+        self.assertEqual(res, 5)
